608b8f375e3165080408d7be79ca3d7900b233fc
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-07-08 18:31:29 -0400

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import CHANGE from "./pages/CHANGE";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <CHANGE />
+      </div>
+    </div>
+  );
+}
+
+export default App;
